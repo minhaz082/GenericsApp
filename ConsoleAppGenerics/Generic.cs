28ad314d9bfdd967ebf68e228d550d3a604ab665
minhaz082 <minhaz082@hotmail.com>
2014-10-31 08:50:07 +0600

    generics example

---
+++ ConsoleAppGenerics/Generic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppGenerics
+{
+    public class Generic<T>
+    {
+
+        int index = 0;
+
+        T[] items;
+         
+        public void Push(T item)
+        {
+            
+        }
+
+        public T POP()
+        {
+            return ;
+        }
+    }
+}
